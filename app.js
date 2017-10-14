35baaa029fd12129b749af89865542e16be7ce41
MichaelLtv <michaelajlam@gmail.com>
2017-10-14 19:15:01 -0400

    added drawing

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 var express = require("express");
 var app = express();
-var serv = require("http").Server(app);
+var server = app.listen(2000);
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/client/index.html");
+app.use(express.static("client"));
 
-});
-app.use("/client", express.static(__dirname + "/client"));
-
-serv.listen(2000);
 console.log("Server started.");
 
-var io = require("socket.io")(serv,{});
-io.sockets.on("connection", function(socket) {
-    socket.id = Math.random();
-    socket.x = 0;
-    socket.y = 0;
-    console.log("socket connection");
-});
+var socket = require("socket.io");
+
+var io = socket(server);
+
+io.sockets.on("connection", newConnection);
+
+function newConnection(socket) {
+    console.log("New connection: " + socket.id);
+
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    }
+}
 
---
+++ client/sketch.js
@@ -0,0 +1,30 @@
+var socket;
+
+function setup() {
+    createCanvas(1280, 960);
+    background(255);
+
+    socket = io.connect("http://localhost:2000");
+    socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+    noStroke();
+    fill(0);
+    ellipse(data.x, data.y, 16, 16);    
+
+}
+
+function mouseDragged() {
+    console.log('Sending: ' + mouseX + ',' + mouseY);
+
+    var data = {
+        x: mouseX,
+        y: mouseY
+    }
+    socket.emit('mouse', data);
+
+    noStroke();
+    fill(0);
+    ellipse(mouseX, mouseY, 16, 16);
+}
