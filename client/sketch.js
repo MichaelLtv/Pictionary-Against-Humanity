d97b5004fb1c7c2c1937a3469bc6c2174607b065
MichaelLtv <michaelajlam@gmail.com>
2017-10-14 22:10:43 -0400

    Changed text

--- client/sketch.js
+++ client/sketch.js
@@ -4,6 +4,14 @@ function setup() {
     createCanvas(960, 1280);
     background(255);
 
+    textSize(48);
+    text("Pictionary Against Humanity", 10, 30);
+    fill(0, 0, 0);
+
+    textSize(24);
+    text("Play Pictionary Against Humanity.", 10, 60);
+    fill(0, 0, 0);
+
     socket = io.connect();
     socket.on('mouse', newDrawing);
 }
